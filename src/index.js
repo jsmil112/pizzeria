db0495f9a7debc134a9ebdfe5c71e1db780ec274
jsmil122 <jsmilbank@gmail.com>
2020-05-24 20:23:35 -0300

    change import name

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { Provider } from "react-redux";
 import { BrowserRouter, Route } from "react-router-dom"
 
-import store from "./redux/store";
+import store from "./redux/store.js";
 
 ReactDOM.render(
   <Provider store={store}>
